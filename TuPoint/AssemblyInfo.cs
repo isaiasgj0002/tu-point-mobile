28fb9bc9a333d767191a70e7d8df166d27563471
isaiasgj0002 <isaiasguzman863@gmail.com>
2024-02-03 19:43:03 -0500

    crar vista de registro

--- TuPoint/AssemblyInfo.cs
+++ TuPoint/AssemblyInfo.cs
@@ -9,3 +9,13 @@ using Xamarin.Forms.Xaml;
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
 [assembly: UsesFeature("android.hardware.location.network", Required = false)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
+// Needed for Picking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+
+// Needed for Taking photo/video
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+
+// Add these properties if you would like to filter out devices that do not have cameras, or set to false to make them optional
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
---
+++ TuPoint/Views/Kilo.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TuPoint.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Kilo : ContentPage
+    {
+        public Kilo()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarImagen_Clicked(object sender, EventArgs e)
+        {
+            // Aquí puedes agregar la lógica para cargar una imagen
+            // Puedes usar la biblioteca Xamarin.Essentials para interactuar con la galería o la cámara.
+            // Ejemplo: https://docs.microsoft.com/en-us/xamarin/essentials/media-picker?tabs=android
+        }
+
+        private void RegistrarKilos_Clicked(object sender, EventArgs e)
+        {
+            // Aquí puedes agregar la lógica para registrar los kilos
+            // Puedes acceder al valor ingresado en el Entry txtKilos.Text
+            string kilosComprobados = txtKilos.Text;
+
+            // Puedes implementar la lógica de registro aquí
+        }
+    }
+}
